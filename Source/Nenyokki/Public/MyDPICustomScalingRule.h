90e9259abb0a87bf9c8d6f762c5bb0b1b4450080
DESKTOP-QTIMD38\aguro <aguroshou0413@gmail.com>
2023-02-04 22:22:05 +0900

    不完全な状態ですが、現状の進捗を共有しておきます。

---
+++ Source/Nenyokki/Public/MyDPICustomScalingRule.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DPICustomScalingRule.h"
+#include "MyDPICustomScalingRule.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NENYOKKI_API UMyDPICustomScalingRule : public UDPICustomScalingRule
+{
+	GENERATED_BODY()
+	
+public:
+	virtual float GetDPIScaleBasedOnSize(FIntPoint Size) const override;
+};
